b5afb583485b4e34cc1c7b3b4ebbf30a5c3ea6d5
dgarascenco <54360508+dgarascenco@users.noreply.github.com>
2021-02-26 15:38:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Request = require("./Request")
 const req = new Request({ url: "dataprovider.example/data"}, function(res) {
-    console.log( res )
+    console.log( res.data )
 })
 req.end()
 
